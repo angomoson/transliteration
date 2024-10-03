762514d3d6e543babd4a3116faa855b350592f84
Angom Oson Singh <angomosn64@gmail.com>
2024-10-03 20:35:31 +0530

    added ipynb file

--- client/main.py
+++ client/main.py
@@ -2,7 +2,8 @@ import requests
 
 url = 'http://localhost:3000/users'  # Ensure the route is correct
 headers = {'Content-Type': 'application/json'}
-data = {'data': 'Chak charabo'}
+inp_text = 'chatle'
+data = {'data': inp_text}
 
 response = requests.post(url, json=data, headers=headers)
 print(response.text)
